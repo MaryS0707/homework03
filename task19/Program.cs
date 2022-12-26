253a8df8f0c110a36c8aa956666dfa434efb4448
Mary <sokolova.mariya.2000@bk.ru>
2022-12-26 15:08:08 +0700

    add task19

---
+++ task19/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+bool func (string num)
+{
+    if (num[0] == num[4] && num[1] == num[3]) return true;
+    else return false;
+}
+
+string number = "23432";
+
+if (func(number)) Console.WriteLine($"{number} -> да");
+else Console.WriteLine($"{number} -> нет");
